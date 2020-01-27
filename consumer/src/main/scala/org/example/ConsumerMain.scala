5e4ca29bb5dfbafc3c37197a4a677bb9600b2cba
pavel_bezglasniy <pavelb23@gmail.com>
2020-01-27 14:36:30 +0300

    Consumer wip

--- consumer/src/main/scala/org/example/ConsumerMain.scala
+++ consumer/src/main/scala/org/example/ConsumerMain.scala
@@ -1,8 +1,14 @@
 package org.example
 
-import org.apache.spark.sql.SparkSession
+import java.net.URLDecoder
+import java.sql.Timestamp
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+import org.apache.spark.sql.expressions.Window
+import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.functions._
-import org.apache.spark.sql.streaming.Trigger
+import org.apache.spark.sql.streaming.{OutputMode, Trigger}
 import org.apache.spark.sql.types.{StringType, StructField, StructType}
 
 
@@ -11,6 +17,45 @@ object ConsumerMain {
   private val columns = List("CONTACTMSG", "DESTADDR", "LOGTYPE", "RESULT", "SMID", "UD", "time")
 
   private val schema = StructType(columns.map(col => StructField(col, StringType)))
+
+  def parseTime(df: DataFrame): DataFrame = {
+    val parseFunc: String => Timestamp = str => Timestamp.valueOf(LocalDateTime.parse(str, DateTimeFormatter.ISO_LOCAL_DATE_TIME))
+    df.withColumn("_time", udf(parseFunc).apply(col("time")))
+  }
+
+  def parseContactMsg(df: DataFrame): DataFrame = {
+    df.
+      withColumn("SMS_ARRAY", split(col("CONTACTMSG"), "-")).
+      withColumn("SMS_NUMBER", expr("SMS_ARRAY[0]")).
+      withColumn("SMS_LEN", expr("SMS_ARRAY[1]")).
+      withColumn("SMS_PART", expr("SMS_ARRAY[2]"))
+  }
+
+  def decodeUrl(df: DataFrame): DataFrame = {
+    val decodeFunc: String => String = url => URLDecoder.decode(url, "UTF8")
+    df.withColumn("URL_DECODED", udf(decodeFunc).apply(col("UD")))
+  }
+
+  def aggreate(df: DataFrame): DataFrame = {
+    df
+      .groupBy(
+        //        window(col("_time"), "5 seconds"),
+        col("DESTADDR"),
+        col("SMS_NUMBER")
+      ).agg(
+      min("_time").as("min_time"),
+      max("_time").as("max_time"),
+      last("SMS_PART").as("LAST_SMS_PART"),
+      last("RESULT").as("LAST_RESULT")
+    )
+  }
+
+  def markerFull(df: DataFrame): DataFrame = {
+    df.withColumn("MARKER_FULL", when(col("SMS_PART") === col("LEN") &&
+      col("RESULT") === lit(0), 1).otherwise(0))
+      //      .filter("MARKER_FULL = 1")
+      .filter(col("MARKER_FULL") === lit(1))
+  }
 
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder().master("local[2]").getOrCreate()
@@ -23,8 +68,14 @@ object ConsumerMain {
       .select(from_json(col("json"), schema).as("parsed"))
       .select("parsed.*").toDF(columns: _*)
 
-    val query = df.writeStream.format("console").
-      trigger(Trigger.ProcessingTime("1 second")).start()
+    val query = df.
+      transform(parseTime).
+      transform(parseContactMsg).
+      transform(decodeUrl).
+      transform(aggreate).
+//      transform(markerFull).
+      writeStream.format("console").outputMode(OutputMode.Complete).
+      trigger(Trigger.ProcessingTime("5 seconds")).start()
     query.awaitTermination()
   }
 }
