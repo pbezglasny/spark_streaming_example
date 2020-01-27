d9b92d7dbd731a3b1a8d8ef4d701d2d546b834d8
pavel_bezglasniy <pavelb23@gmail.com>
2020-01-27 17:59:22 +0300

    Added spark ml pipeline.

--- spark_ml/spark_ml_demo.py
+++ spark_ml/spark_ml_demo.py
@@ -1,8 +1,41 @@
-from pyspark.sql import *
+from pyspark.ml import Pipeline
+from pyspark.ml.classification import RandomForestClassifier
+from pyspark.ml.evaluation import BinaryClassificationEvaluator
+from pyspark.ml.feature import *
+from pyspark.ml.tuning import ParamGridBuilder, CrossValidator
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
 
 
 def main():
     spark = SparkSession.builder.master("local[2]").getOrCreate()
+    df = spark.read.option("header", "true").option("inferSchema", "true").csv("data/train.csv"). \
+        withColumnRenamed("Survived", "label")
+    avg_age = df.agg(mean("Age").alias("avg")).collect()[0]["avg"]
+    na_map = {"Age": avg_age, "Sex": "male", "Embarked": "S", "Fare": 0.0, "SibSp": 0.0}
+    df = df.na.fill(na_map)
+    sex_indexer = StringIndexer().setInputCol(value="Sex").setOutputCol("sex_index")
+    embarked_indexer = StringIndexer().setInputCol("Embarked").setOutputCol("embarked_index")
+    vector_assembler = VectorAssembler().setInputCols(["sex_index", "embarked_index", "Fare", "SibSp", "Age"]). \
+        setOutputCol("features")
+    classifier = RandomForestClassifier().setFeaturesCol("features")
+
+    pipeline = Pipeline().setStages([sex_indexer, embarked_indexer, vector_assembler, classifier])
+
+    grid = ParamGridBuilder().addGrid(classifier.maxDepth, [5, 8]). \
+        addGrid(classifier.maxBins, [16, 32]).build()
+
+    crossval = CrossValidator(estimator=pipeline,
+                              estimatorParamMaps=grid,
+                              evaluator=BinaryClassificationEvaluator(),
+                              numFolds=3)
+
+    cv_model = crossval.fit(df)
+
+    test_df = spark.read.option("header", "true").option("inferSchema", "true").csv("data/test.csv").fillna(na_map)
+    cv_model.transform(test_df). \
+        select("PassengerId", "prediction"). \
+        show()
 
 
 if __name__ == '__main__':
